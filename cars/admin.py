77b613b19635bd9666ca12081fe0d38a48f72a56
Shruti Sharma <sharmas23032001@gmail.com>
2021-11-13 09:20:47 +0530

    Added Cars App

---
+++ cars/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Car
+# Register your models here.
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('car_title','color','model','year','body_style','fuel_type','is_featured')
+admin.site.register(Car,CarAdmin)
