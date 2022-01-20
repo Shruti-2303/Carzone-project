8a2db475c70b32844d3e73f3278c522fb5ca53cd
Shruti Sharma <sharmas23032001@gmail.com>
2022-01-20 22:52:44 +0530

    Admin Customization added

--- carzone/settings.py
+++ carzone/settings.py
@@ -31,15 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pages.apps.PagesConfig',
+    'cars.apps.CarsConfig',
+    'ckeditor',
+    'django.contrib.staticfiles',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'pages.apps.PagesConfig',
-    'cars.apps.CarsConfig',
-    'ckeditor',
     'django.contrib.humanize'
 ]
 
